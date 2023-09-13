2a2198986217df4d7c112ea7e5a5198b281ba312
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-09-13 16:17:35 -0300

    first commit

---
+++ src/components/Titulo/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function Titulo(props) {
+
+  return (
+    <div className="d-flex flex-start bg-dark p-3">
+      <h1 className="pm">Profetizando Milagres</h1>
+    </div>
+  );
+}
+
+export default Titulo;
