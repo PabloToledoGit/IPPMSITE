2b7ba2bac4638efd4e5d57bdd02d5fccd737a7cb
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-09-15 15:34:15 -0300

    criando carrosel

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,8 +1,11 @@
 import "./index.css";
 
 function Menu(props) {
-  return <a className="text-warning link btn btn-neon" href={props.links}>{props.menu}</a>;
+  return (
+    <a className="text-warning link" href={props.links}>
+      {props.menu}
+    </a>
+  );
 }
-
 
 export default Menu;
---
+++ src/components/MenuHamburguer/index.js
@@ -0,0 +1,11 @@
+import './MenuHamburguer';
+
+function Hamburguer (){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Hamburguer;
