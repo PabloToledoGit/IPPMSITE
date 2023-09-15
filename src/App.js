8d6725a3007eaa52f30932ebf6520d28c8d6e925
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-09-15 15:37:53 -0300

    ajustando

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
           <Menu menu="Live" links="https://ippm.vercel.app/" />
         </div>
       </div>
-      <Titulo titulo="Profetizando Milagres" />
     </div>
   );
 }
